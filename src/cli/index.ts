9e4d962e7df9cf56bd14797be971a1cf5172ba1d
Mr.Worldwide <syahfis@gmail.com>
2026-03-24 01:15:43 +0800

    feat(app): cli delivery schedule by accept vehicle config

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,9 +1,12 @@
 import * as readline from 'readline';
 import { StdinParser } from '../infrastructure/parsers/StdinParser/StdinParser';
 import { CostFormatter } from '../infrastructure/formatters/CostFormatter';
+import { DeliveryFormatter } from '../infrastructure/formatters/DeliveryFormatter';
 import { InMemoryOfferRepository } from '../features/delivery-cost/repositories/InMemoryOfferRepository/InMemoryOfferRepository';
 import { OfferService } from '../features/delivery-cost/services/OfferService/OfferService';
 import { CalculateDeliveryCost } from '../features/delivery-cost/CalculateDeliveryCost/CalculateDeliveryCost';
+import { ShipmentSelector } from '../features/delivery-schedule/ShipmentSelector/ShipmentSelector';
+import { DeliveryScheduler } from '../features/delivery-schedule/DeliveryScheduler/DeliveryScheduler';
 
 const rl = readline.createInterface({ input: process.stdin });
 const lines: string[] = [];
@@ -11,15 +14,35 @@ const lines: string[] = [];
 rl.on('line', (line) => lines.push(line.trim()));
 
 rl.on('close', () => {
+  // infrastructure
   const parser = new StdinParser();
   const costFormatter = new CostFormatter();
+  const deliveryFormatter = new DeliveryFormatter();
+
+  // delivery cost
   const offerRepo = new InMemoryOfferRepository();
   const offerService = new OfferService(offerRepo);
   const calculateDeliveryCost = new CalculateDeliveryCost(offerService);
 
-  const { baseCost, packages } = parser.parse(lines);
-  const results = calculateDeliveryCost.execute(baseCost, packages);
-  const output = costFormatter.formatCosts(results);
+  // delivery schedule
+  const selector = new ShipmentSelector();
+  const scheduler = new DeliveryScheduler(selector);
 
-  output.forEach((line) => console.log(line));
+  // parse
+  const { baseCost, packages, vehicleConfig } = parser.parse(lines);
+
+  // calculate costs
+  const costResults = calculateDeliveryCost.execute(baseCost, packages);
+
+  // problem 01
+  if (!vehicleConfig) {
+    costFormatter.formatCosts(costResults).forEach((line) => console.log(line));
+    return;
+  }
+
+  // problem 02
+  const deliveryResults = scheduler.schedule(packages, vehicleConfig);
+  deliveryFormatter
+    .formatDeliveries(costResults, deliveryResults)
+    .forEach((line) => console.log(line));
 });
