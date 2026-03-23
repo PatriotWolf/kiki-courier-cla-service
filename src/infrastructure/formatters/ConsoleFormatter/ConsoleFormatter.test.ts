c0358ba831ca9682e6237ddf28073c0995df6c7f
Mr.Worldwide <syahfis@gmail.com>
2026-03-23 21:41:31 +0800

    test(formatter): infrastructure formatter ConsoleFormatter test todo

---
+++ src/infrastructure/formatters/ConsoleFormatter/ConsoleFormatter.test.ts
@@ -0,0 +1,7 @@
+describe('ConsoleFormatter', () => {
+  describe('problem 01 output', () => {
+    it.todo('formats package id discount and total cost');
+    it.todo('formats zero discount correctly');
+    it.todo('formats multiple results in order');
+  });
+});
